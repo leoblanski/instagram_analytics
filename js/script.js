8eb1ed6e319c27441104a1a3a6364de6c78df356
Leonardo <leo_blanski@hotmail.com>
2021-10-01 21:23:17 -0300

    Alteraçoes

--- js/script.js
+++ js/script.js
@@ -3,13 +3,11 @@ $(document).ready(function(){
 
   $("#process_list").on("click", function(){
     
-    array_accounts = $("#array_accounts").val();
+    array_accounts = {$("#array_accounts").val())};
 
-
-    array_accounts.forEach(function(i){
+    array_accounts.each(function(i, e){
       console.log(i);
     })
-
 
   });
 
